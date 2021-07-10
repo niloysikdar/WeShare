8464d6f5f904cceeda7e670b9d6c7b3bfe13ae56
niloysikdar <niloysikdar30@gmail.com>
2021-07-11 04:42:08 +0530

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.json({ limit: "25mb" }));
+app.use(cors());
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
