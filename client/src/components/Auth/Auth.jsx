35d24748bd82c74897f198ae1bddcbd4e1c61fa9
niloysikdar <niloysikdar30@gmail.com>
2021-07-17 23:21:48 +0530

    Added Google OAuth

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,9 +1,115 @@
+import { useState } from "react";
+import {
+  Container,
+  Grid,
+  Paper,
+  Typography,
+  Avatar,
+  Button,
+} from "@material-ui/core";
+import LockOutlined from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
+import InputField from "./InputField";
+import GoogleLoginButton from "./GoogleLoginButton";
+
 const Auth = () => {
-  const state = null;
+  const classes = useStyles();
+  const [isShowPassword, setShowPassword] = useState(true);
+  const [isSignup, setisSignup] = useState(false);
+
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
+  const handleIsShowPassword = () => {
+    setShowPassword(!isShowPassword);
+  };
+
   return (
-    <div>
-      <h2>Auth Page</h2>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlined />
+        </Avatar>
+        <Typography variant="h5"> {isSignup ? "Sign Up" : "Login"} </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <InputField
+                  name="firstName"
+                  label="First Name"
+                  type="text"
+                  half
+                  handleChange={handleChange}
+                />
+                <InputField
+                  name="lastName"
+                  label="Last Name"
+                  type="text"
+                  half
+                  handleChange={handleChange}
+                />
+              </>
+            )}
+            <InputField
+              name="email"
+              label="Email"
+              type="email"
+              handleChange={handleChange}
+            />
+            <InputField
+              name="password"
+              label="Password"
+              type={isShowPassword ? "password" : "text"}
+              handleShowPassword={handleIsShowPassword}
+              handleChange={handleChange}
+            />
+            {isSignup && (
+              <InputField
+                name="password"
+                label="Confirm Password"
+                type={isShowPassword ? "password" : "text"}
+                handleShowPassword={handleIsShowPassword}
+                handleChange={handleChange}
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            className={classes.submit}
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            {isSignup ? "Sign Up" : "Login"}
+          </Button>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button
+                style={{ textTransform: "none" }}
+                onClick={() => {
+                  setisSignup(!isSignup);
+                }}
+              >
+                {isSignup
+                  ? "Already have an account? Login"
+                  : "New User? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+      <GoogleLoginButton isSignup={isSignup} />
+    </Container>
   );
 };
 
---
+++ client/src/components/Auth/GoogleLoginButton.jsx
@@ -0,0 +1,47 @@
+import { GoogleLogin } from "react-google-login";
+import { Button } from "@material-ui/core";
+import dotenv from "dotenv";
+import useStyles from "./styles";
+
+dotenv.config();
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const GoogleLoginButton = ({ isSignup }) => {
+  const classes = useStyles();
+
+  const googleSuccess = async (res) => {
+    const tokenId = res?.tokenId;
+    const result = res?.profileObj;
+    try {
+      console.log({ result, tokenId });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const googleFailure = () => {
+    console.log("Failure");
+  };
+
+  return (
+    <GoogleLogin
+      clientId={GOOGLE_CLIENT_ID}
+      render={(renderProps) => (
+        <Button
+          className={classes.googleButton}
+          variant="contained"
+          color="secondary"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          {isSignup ? "Sign Up with Google" : "Login with Google"}
+        </Button>
+      )}
+      onSuccess={googleSuccess}
+      onFailure={googleFailure}
+      cookiePolicy="single_host_origin"
+    ></GoogleLogin>
+  );
+};
+
+export default GoogleLoginButton;
